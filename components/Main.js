ad5a0344ab01a0a5d92a8494c9d8f7fbc3d25993
minhquan0902 <minhquan0902@gmail.com>
2021-05-10 18:04:35 +0800

    Displaying User Profile With Firebase Firesotre

--- components/Main.js
+++ components/Main.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { fetchUser } from "../redux/actions/index";
+import { fetchUser, fetchUserPosts } from "../redux/actions/index";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import FeedScreen from "./main/Feed";
 import ProfileScreen from "./main/Profile";
@@ -15,6 +15,7 @@ const EmptyScreen = () => {
 export class Main extends Component {
   componentDidMount() {
     this.props.fetchUser();
+    this.props.fetchUserPosts();
   }
   render() {
     return (
@@ -65,5 +66,5 @@ const mapStateToProps = (store) => ({
   currentUser: store.userState.currentUser,
 });
 const mapDispatchProps = (dispatch) =>
-  bindActionCreators({ fetchUser }, dispatch);
+  bindActionCreators({ fetchUser, fetchUserPosts }, dispatch);
 export default connect(mapStateToProps, mapDispatchProps)(Main);
--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from "firebase";
-import { USER_STATE_CHANGE } from "../constants/index";
+import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE } from "../constants/index";
 require("firebase/firestore");
 export function fetchUser() {
   return (dispatch) => {
@@ -14,6 +14,31 @@ export function fetchUser() {
         } else {
           console.log("does not exist");
         }
+      });
+  };
+}
+
+export function fetchUserPosts() {
+  return (dispatch) => {
+    firebase
+      .firestore()
+      .collection("posts")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("userPosts")
+      .orderBy("creation", "asc")
+      .get()
+      .then((snapshot) => {
+        let posts = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          const id = doc.id;
+
+          return { id, ...data };
+        });
+        console.log(posts);
+        dispatch({
+          type: USER_POSTS_STATE_CHANGE,
+          posts,
+        });
       });
   };
 }
