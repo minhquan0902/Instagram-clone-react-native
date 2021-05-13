f4b4cfa292dc4f339913c02386cd61f548e16c5a
minhquan0902 <minhquan0902@gmail.com>
2021-05-14 02:58:47 +0800

    Post Feed System with Firebase and React Native

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -3,6 +3,8 @@ import {
   USER_STATE_CHANGE,
   USER_POSTS_STATE_CHANGE,
   USER_FOLLOWING_STATE_CHANGE,
+  USERS_DATA_STATE_CHANGE,
+  USERS_POSTS_STATE_CHANGE,
 } from "../constants/index";
 require("firebase/firestore");
 export function fetchUser() {
@@ -64,6 +66,69 @@ export function fetchUserFollowing() {
           type: USER_FOLLOWING_STATE_CHANGE,
           following,
         });
+        for (let i = 0; i < following.length; i++) {
+          dispatch(fetchUsersData(following[i]));
+        }
+      });
+  };
+}
+
+export function fetchUsersData(uid) {
+  return (dispatch, getState) => {
+    const found = getState().usersState.users.some((el) => el.uid === uid);
+
+    if (!found) {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(uid)
+        .get()
+        .then((snapshot) => {
+          if (snapshot.exists) {
+            let user = snapshot.data();
+            user.uid = snapshot.id;
+
+            dispatch({
+              type: USERS_DATA_STATE_CHANGE,
+              user,
+            });
+            dispatch(fetchUsersFollowingPosts(user.uid));
+          } else {
+            console.log("does not exist");
+          }
+        });
+    }
+  };
+}
+
+export function fetchUsersFollowingPosts(uid) {
+  return (dispatch, getState) => {
+    firebase
+      .firestore()
+      .collection("posts")
+      .doc(uid)
+      .collection("userPosts")
+      .orderBy("creation", "asc")
+      .get()
+      .then((snapshot) => {
+        const uid = snapshot._.query.C_.path.segments[1];
+        console.log({ snapshot, uid });
+
+        const user = getState().usersState.users.find((el) => el.uid === uid);
+
+        let posts = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          const id = doc.id;
+
+          return { id, ...data, user };
+        });
+        console.log(posts);
+        dispatch({
+          type: USERS_POSTS_STATE_CHANGE,
+          posts,
+          uid,
+        });
+        console.log(getState());
       });
   };
 }
